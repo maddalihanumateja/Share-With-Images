2edebfd57918dd60518499505de19668688fe605
Hanuma Teja Maddali <hanumatejamaddali@wireless-10-105-213-108.umd.edu>
2018-03-11 19:43:12 -0400

    Added an image attribute to the SharingImage object. The user can select some image from the photo library. Need to modify this in the future to allow them to snap photos. When the sharingImageDescription text field is first responder the keyboard covers the text field.

--- SharingImageViewController.swift
+++ SharingImageViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class SharingImageViewController: UIViewController, UITextFieldDelegate {
+class SharingImageViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     
     @IBOutlet weak var sharingImageNameLabel: UILabel!
     @IBOutlet weak var sharingImageNameTextField: UITextField!
     @IBOutlet weak var sharingImageDescriptionTextField: UITextField!
+    @IBOutlet weak var sharingImagePhotoView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +55,45 @@ class SharingImageViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        sharingImagePhotoView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
     //MARK: Actions
-
+    
+    @IBAction func selectSharingImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        sharingImageNameTextField.resignFirstResponder()
+        sharingImageDescriptionTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
 }
