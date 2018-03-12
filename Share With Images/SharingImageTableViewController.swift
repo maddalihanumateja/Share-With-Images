b385758ea66537bb9ca38e52d98afccb4e2aba55
Hanuma Teja Maddali <hanumatejamaddali@wireless-10-105-213-108.umd.edu>
2018-03-12 12:44:00 -0400

    Users cand delete SharingImage objects

--- Share With Images/SharingImageTableViewController.swift
+++ Share With Images/SharingImageTableViewController.swift
@@ -18,6 +18,9 @@ class SharingImageTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         // Load the sample data.
         loadSampleSharingImages()
     }
@@ -72,17 +75,16 @@ class SharingImageTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            sharingImages.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -91,13 +93,11 @@ class SharingImageTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
 
     // MARK: - Navigation
 
