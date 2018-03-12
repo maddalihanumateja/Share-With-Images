80c30507e2f93c2cfbc71133f0a290f4a5d65eb7
Hanuma Teja Maddali <hanumatejamaddali@wireless-10-105-213-108.umd.edu>
2018-03-11 22:25:47 -0400

    Created a SharingImage Table and TableCell views

---
+++ Share With Images/SharingImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SharingImageTableViewCell.swift
+//  Share With Images
+//
+//  Created by Hanuma Teja Maddali on 3/11/18.
+//  Copyright © 2018 Hanuma Teja Maddali. All rights reserved.
+//
+
+import UIKit
+
+class SharingImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
