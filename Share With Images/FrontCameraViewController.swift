d6d82f417fef6794c6b99662aa68a11382b1ee8e
Hanuma Teja Maddali <hanumatejamaddali@wireless-10-105-211-244.umd.edu>
2018-03-27 18:38:29 -0400

    Completed rectangle detection pipeline. Draws one of the detected rectangles on screen by manipulating the position of an existing rectangle object

--- Share With Images/FrontCameraViewController.swift
+++ Share With Images/FrontCameraViewController.swift
@@ -1,18 +1,25 @@
 //
-//  ViewController.swift
-//  ObjectTracker
-//
-//  Created by Jeffrey Bergier on 6/8/17.
-//  Copyright © 2017 Saturday Apps. All rights reserved.
+//  FrontCameraViewControllerViewController.swift
 //
 
 import AVFoundation
 import Vision
 import UIKit
+import os.log
 
 class FrontCameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
+    //MARK: Properties
+    
     @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var highlightView: UIView!{
+        didSet {
+            self.highlightView?.layer.borderColor = UIColor.red.cgColor
+            self.highlightView?.layer.borderWidth = 4
+            self.highlightView?.backgroundColor = .clear
+        }
+    }
+    
     
     //Hide the default status bar in this view
     override var prefersStatusBarHidden: Bool {
@@ -20,11 +27,12 @@ class FrontCameraViewController: UIViewController, AVCaptureVideoDataOutputSampl
     }
     
     private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    
     private lazy var captureSession: AVCaptureSession = {
         let session = AVCaptureSession()
         session.sessionPreset = AVCaptureSession.Preset.photo
         guard
-            let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front),
+            let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
             let input = try? AVCaptureDeviceInput(device: frontCamera)
             else { return session }
         session.addInput(input)
@@ -36,6 +44,9 @@ class FrontCameraViewController: UIViewController, AVCaptureVideoDataOutputSampl
         
         // make the camera appear on the screen
         self.cameraView?.layer.addSublayer(self.cameraLayer)
+        
+        // hide the red focus area on load
+        self.highlightView?.frame = .zero
         
         // register to receive buffers from the camera
         let videoOutput = AVCaptureVideoDataOutput()
@@ -54,12 +65,45 @@ class FrontCameraViewController: UIViewController, AVCaptureVideoDataOutputSampl
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard
             // make sure the pixel buffer can be converted
-        let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+            let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+            else { return }
+        
+        var requestOptions: [VNImageOption: Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics: cameraIntrinsicData]
+        }
+        let visionImageHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: requestOptions)
+        let rectFinderRequest = VNDetectRectanglesRequest(completionHandler: handleRectRequest)
+        rectFinderRequest.minimumConfidence = 0.7
+        rectFinderRequest.maximumObservations = 1
+        do {
+            try visionImageHandler.perform([rectFinderRequest])
+        } catch {
+            print(error)
+        }
     }
     
-    private func handleVisionRequestUpdate(_ request: VNRequest, error: Error?) {
+    private func handleRectRequest(_ request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+        // make sure we have an actual result
+        guard let newObservation = request.results?.first as? VNRectangleObservation else {
+            // hide the red focus area
+            self.highlightView?.frame = .zero
+            return }
+        //for observation in (request.results) {
+            //guard let rectObservation = observation as! VNRectangleObservation else {return}
+            var rectBoundingBox = newObservation.boundingBox
+            //rectBoundingBox.origin.y = 1 - rectBoundingBox.origin.y
+            //rectBoundingBox.origin.x = 1 - rectBoundingBox.origin.x
+            print(rectBoundingBox)
+        let convertedRect = self.cameraLayer.layerRectConverted(fromMetadataOutputRect: rectBoundingBox)
         
+        // move the highlight view
+        self.highlightView?.frame = convertedRect
+        //}
+        }
     }
     
 }
